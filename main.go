480b8f8bcea2f4b43ace4c95c621919e479d9fa0
el10savio <el10savio96@gmail.com>
2021-10-07 12:27:00 +0530

    Added Main Package

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+
+	"handlers"
+)
+
+const (
+	// PORT ...
+	PORT = "8080"
+)
+
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+}
+
+func main() {
+	r := handlers.Router()
+
+	log.WithFields(log.Fields{
+		"port": PORT,
+	}).Info("started set node server")
+
+	http.ListenAndServe(":"+PORT, r)
+}
