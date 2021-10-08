0fb95e4fbf44ab61977d9e1920244c831528ce59
el10savio <el10savio96@gmail.com>
2021-10-08 13:49:52 +0530

    Added Get Handler Tests

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -27,32 +27,43 @@ func TestIndex(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
+	defer testClearUtil(t)
 	testAddUtil(t, "1", http.StatusOK)
 }
 
 func TestAdd_NonNumberElement(t *testing.T) {
+	defer testClearUtil(t)
 	testAddUtil(t, "+", http.StatusInternalServerError)
 }
 
 func TestAdd_DuplicateElement(t *testing.T) {
+	defer testClearUtil(t)
 	testAddUtil(t, "1", http.StatusOK)
 	testAddUtil(t, "1", http.StatusOK)
 }
 
 func TestGet(t *testing.T) {
+	defer testClearUtil(t)
 	testAddUtil(t, "1", http.StatusOK)
 	testGetUtil(t, "[1]\n", http.StatusOK)
 }
 
-// func TestGet_NonNumberElement(t *testing.T) {
-// 	testAddUtil(t, "+", http.StatusInternalServerError)
-// 	testGetUtil(t, "", http.StatusOK)
-// }
+func TestGet_NonNumberElement(t *testing.T) {
+	defer testClearUtil(t)
+	testAddUtil(t, "+", http.StatusInternalServerError)
+	testGetUtil(t, "[]\n", http.StatusOK)
+}
 
-// func TestGet_DuplicateElement(t *testing.T) {
-// 	testAddUtil(t, "1", http.StatusOK)
-// 	testAddUtil(t, "1", http.StatusOK)
-// }
+func TestGet_EmptyElement(t *testing.T) {
+	defer testClearUtil(t)
+	testGetUtil(t, "[]\n", http.StatusOK)
+}
+
+func TestGet_DuplicateElement(t *testing.T) {
+	defer testClearUtil(t)
+	testAddUtil(t, "1", http.StatusOK)
+	testAddUtil(t, "1", http.StatusOK)
+}
 
 func testAddUtil(t *testing.T, element string, expectedStatus int) {
 	payload := fmt.Sprintf(`{"value": "%s"}`, element)
@@ -81,4 +92,18 @@ func testGetUtil(t *testing.T, expectedList string, expectedStatus int) {
 
 	assert.Equal(t, expectedStatus, rr.Code)
 	assert.Equal(t, expectedList, rr.Body.String())
+}
+
+func testClearUtil(t *testing.T) {
+	request, err := http.NewRequest("GET", "/set/debug/clear", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Clear)
+	handler.ServeHTTP(rr, request)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+	testGetUtil(t, "[]\n", http.StatusOK)
 }
